2be5762b5a7aa89688a2034aad2308630b838084
Sarath J Nair <sarathjnair@Saraths-MacBook-Pro.local>
2024-02-22 11:48:09 +0100

    [] Initial Commit

---
+++ src/main/java/com/springboot/cred/CredApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.cred;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CredApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CredApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springboot/cred/CredApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.cred;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CredApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
